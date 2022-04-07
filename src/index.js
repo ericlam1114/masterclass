088627b59f57488efcf592cd122929ddfb60df38
ericlam1114 <83894250+ericlam1114@users.noreply.github.com>
2022-04-07 15:08:09 -0700

    add serviceWorker.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import ThemeContextProvider from "./contexts/ThemeContext";
+import * as serviceWorker from './serviceWorker';
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -15,4 +16,5 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-reportWebVitals();
+reportWebVitals()
+serviceWorker.unregister();
